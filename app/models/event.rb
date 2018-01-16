dcc60babedaf5127c2059655a5a43e70da2ea0b7
LilianKalkman <liliankalkman@hotmail.com>
2018-01-16 13:57:18 +0100

    Controller actions

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,7 @@ class Event < ApplicationRecord
 
     validates :name, presence: true
     validates :description, presence: true, length: { maximum: 500 }
+    # validates :starts_at, presence: true
+    # validates :ends_at, presence: true, date: { after_or_equal_to:  :starts_at}
 
 end
