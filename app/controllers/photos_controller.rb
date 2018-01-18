aeaf14bdfc8bbdbb08de8101a4f08cc487a7ea68
LilianKalkman <liliankalkman@hotmail.com>
2018-01-18 16:31:59 +0100

    1631

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 
   def destroy
     photo = Photo.find(params[:id])
-    @room = photo.room
-    if @room.user.id == current_user.id
+    @event = photo.event
+    if @event.user.id == current_user.id
       photo.destroy
 
-      redirect_to edit_room_path(@room), notice: "Photo successfully removed"
+      redirect_to edit_event_path(@event), notice: "Photo successfully removed"
     else
-      redirect_to @room, notice: "Cannot delete that photo"
+      redirect_to @event, notice: "Cannot delete that photo"
     end
   end
 
