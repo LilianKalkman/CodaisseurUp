1b4306dbede3fce643974b6b744623d41c44a9cd
LilianKalkman <liliankalkman@hotmail.com>
2018-01-18 15:51:46 +0100

    foto dingen

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,16 @@
 class PhotosController < ApplicationController
+
+
+  def destroy
+    photo = Photo.find(params[:id])
+    @room = photo.room
+    if @room.user.id == current_user.id
+      photo.destroy
+
+      redirect_to edit_room_path(@room), notice: "Photo successfully removed"
+    else
+      redirect_to @room, notice: "Cannot delete that photo"
+    end
+  end
+
 end
